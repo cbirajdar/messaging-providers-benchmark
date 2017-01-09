aebbcd5f6a03a9f3e26e163d00de9f5a51465a34
Chetan Birajdar <birajdar.chetan@gmail.com>
2017-01-09 12:02:44 -0800

    Refactor RabbitMqbroker

--- src/main/java/demo/benchmark/RabbitMqMessageBroker.java
+++ src/main/java/demo/benchmark/RabbitMqMessageBroker.java
@@ -4,6 +4,7 @@ import com.rabbitmq.client.*;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.IntStream;
 
 public class RabbitMqMessageBroker extends AbstractMessageBroker {
 
@@ -23,22 +24,39 @@ public class RabbitMqMessageBroker extends AbstractMessageBroker {
         channel.queueDeclare(QUEUE, false, false, false, null);
     }
 
-    @Override public void enqueue() throws IOException {
-        log().info("Sending....");
-        for (int i = 0; i < enqueue_count; i++) {
-            channel.basicPublish("", QUEUE, null, String.valueOf(i).getBytes());
+    @Override public void enqueue() {
+        Runnable runnable = () -> {
+            IntStream.range(0, enqueue_count).forEach(i -> publish(String.valueOf(i)));
+        };
+        submit(producerThreads, runnable);
+    }
+
+    private void publish(String data) {
+        try {
+            channel.basicPublish("", QUEUE, null, String.valueOf(data).getBytes());
+        } catch (IOException e) {
+            log().error("Error publishing data to the queue", e);
         }
     }
 
     @Override public void dequeue() throws IOException {
-        log().info("Receiving....");
         Consumer consumer = new DefaultConsumer(channel);
-        for (int i = 0; i < enqueue_count; i++) {
+        Runnable runnable = () -> {
+            IntStream.range(0, enqueue_count).forEach(i -> consume(consumer));
+        };
+        submit(consumerThreads, runnable);
+    }
+
+    private void consume(Consumer consumer) {
+        try {
             channel.basicConsume(QUEUE, true, consumer);
+        } catch (IOException e) {
+            log().error("Error consuming data from the queue", e);
         }
     }
 
     @Override public void closeConnection() throws IOException, TimeoutException {
+        waitForThreadPoolTermination();
         channel.close();
         connection.close();
     }
