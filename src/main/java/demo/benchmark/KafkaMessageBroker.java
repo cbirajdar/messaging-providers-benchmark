005dfa42b1989598dcb1621f3d553b5be6356c14
Chetan Birajdar <birajdar.chetan@gmail.com>
2017-01-09 12:02:45 -0800

    Config changes for kafka

--- src/main/java/demo/benchmark/KafkaMessageBroker.java
+++ src/main/java/demo/benchmark/KafkaMessageBroker.java
@@ -1,49 +1,71 @@
 package demo.benchmark;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Properties;
 
 public class KafkaMessageBroker extends AbstractMessageBroker {
 
-    private KafkaProducer<String, String> producer;
+    private KafkaProducer<Integer, String> producer;
 
-    private KafkaConsumer<String, String> consumer;
+    private KafkaConsumer<Integer, String> consumer;
 
     public KafkaMessageBroker(String port) {
         createConnection(port);
     }
 
     @Override public void createConnection(String port) {
+        createProducer(port);
+        createConsumer(port);
+    }
+
+    private void createProducer(String port) {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:" + port);
-        props.put("group.id", "TestExample");
-        props.put("key.serializer", StringSerializer.class);
-        props.put("value.serializer", StringSerializer.class);
-        props.put("key.deserializer", StringDeserializer.class);
-        props.put("value.deserializer", StringDeserializer.class);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:" + port);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "TestProducer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         producer = new KafkaProducer<>(props);
+    }
+
+    private void createConsumer(String port) {
+        /*
+         * Properties based on example the following example,
+         * https://github.com/apache/kafka/blob/trunk/examples/src/main/java/kafka/examples/Consumer.java
+         */
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:" + port);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "TestConsumer");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "10000");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         consumer = new KafkaConsumer<>(props);
     }
 
     @Override public void enqueue() {
-        Runnable runnable = () -> stream(enqueue_count, i -> producer.send(new ProducerRecord<>(QUEUE, "Test", "Test")), "Enqueue");
+        Runnable runnable = () -> stream(enqueue_count, i -> producer.send(new ProducerRecord<>(QUEUE, i, "Test")), "Enqueue");
         submit(producerThreads, runnable);
     }
 
     @Override public void dequeue() {
-        consumer.subscribe(Arrays.asList(QUEUE));
+        consumer.subscribe(Collections.singletonList(QUEUE));
         Runnable runnable = () -> {
-            ConsumerRecords<String, String> records = consumer.poll(1000);
-            for (ConsumerRecord<String, String> record : records) {
-                record.value(); //Do something with the value
+            ConsumerRecords<Integer, String> records = consumer.poll(1000);
+            for (ConsumerRecord<Integer, String> record : records) {
+                log().info(record.value()); //Do something with the value
             }
         };
         submit(consumerThreads, runnable);
